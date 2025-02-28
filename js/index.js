443f25c9a4f6e7a7e0db516a695ad1190375a5fb
DexStephens <dexterfstephens@gmail.com>
2025-02-27 23:16:43 -0700

    Make sure the svg works in a relative container for adding line positions correctly, and make sure that the svg grows as necessary when new content is added.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 function circleClick(event) {
+  const board = document.getElementById("board");
+  const boardRect = board.getBoundingClientRect();
+
+  const svg = document.getElementById("svgCanvas");
+  const svgRect = svg.getBoundingClientRect();
+
   const parentRect = event.target.parentElement.getBoundingClientRect();
   const rect = event.target.getBoundingClientRect();
 
   const classList = Array.from(event.target.classList);
 
-  const circleTop = rect.top + Math.floor(rect.height / 2);
-  const circleLeft = rect.left + Math.floor(rect.width / 2);
+  const circleTop = rect.top + Math.floor(rect.height / 2) - svgRect.top;
+  const circleLeft = rect.left + Math.floor(rect.width / 2) - svgRect.left;
 
   const lineTop = classList.find(
     (clazz) => clazz === "top" || clazz === "bottom"
@@ -19,18 +25,20 @@ function circleClick(event) {
     ? calculateNew(classList.includes("right"), circleLeft, 100)
     : circleLeft;
 
+  if (lineTop + 100 > svgRect.height || lineLeft + 100 > svgRect.width) {
+    resizeSvgCanvas();
+  }
+
   drawLine(circleLeft, circleTop, lineLeft, lineTop);
 
-  const squareTop = classList.find(
-    (clazz) => clazz === "top" || clazz === "bottom"
-  )
-    ? calculateNew(classList.includes("bottom"), parentRect.top, 200)
-    : parentRect.top;
-  const squareLeft = classList.find(
-    (clazz) => clazz === "left" || clazz === "right"
-  )
-    ? calculateNew(classList.includes("right"), parentRect.left, 200)
-    : parentRect.left;
+  const squareTop =
+    (classList.find((clazz) => clazz === "top" || clazz === "bottom")
+      ? calculateNew(classList.includes("bottom"), parentRect.top, 200)
+      : parentRect.top) - svgRect.top;
+  const squareLeft =
+    (classList.find((clazz) => clazz === "left" || clazz === "right")
+      ? calculateNew(classList.includes("right"), parentRect.left, 200)
+      : parentRect.left) - svgRect.left;
 
   event.target.parentElement.parentElement.appendChild(
     addSquare(squareTop, squareLeft)
@@ -52,7 +60,6 @@ function addSquare(top, left) {
 }
 
 function drawLine(x1, y1, x2, y2) {
-  console.log(x1, x2, y1, y2);
   const svg = document.getElementById("svgCanvas");
 
   const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
@@ -77,6 +84,13 @@ function setupSvgCanvas() {
 
   svg.setAttribute("width", boardWidth);
   svg.setAttribute("height", boardHeight);
+}
+
+function resizeSvgCanvas() {
+  const svg = document.getElementById("svgCanvas");
+
+  svg.setAttribute("width", svg.width.baseVal.value + 500);
+  svg.setAttribute("height", svg.height.baseVal.value + 500);
 }
 
 export function startup() {
